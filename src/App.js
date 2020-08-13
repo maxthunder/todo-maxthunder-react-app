bf3b4c91abae8cf5e70dbee2cb5cd1eec0d64af4
maxthunder <maxwell.stark@gmail.com>
2020-08-12 23:29:00 -0600

    adds skeleton code from angular version.

--- src/App.js
+++ src/App.js
@@ -4,22 +4,46 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <!-- Always shows a header, even in smaller screens. -->
+      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header" style="background: #000000;">
+        <header className="mdl-layout__header" style="background-color: #FFD034">
+          <div className="mdl-layout__header-row">
+            <!-- Title -->
+            <span className="task-title mdl-layout-title mdl-layout--small-screen-only" style="padding-bottom: 10px;"><a className="mdl-navigation__link"><img src="../assets/maxthunder-logo.png" height="48" width="48"/></a></span>
+            <span className="task-title mdl-layout-title mdl-layout--large-screen-only" style="padding-bottom: 10px;"><a className="mdl-navigation__link"><img src="../assets/maxthunder-todolist-logo.png" height="48" width="458"/></a></span>
+            <!-- Add spacer, to align navigation to the right -->
+            <div className="mdl-layout-spacer"></div>
+            <!-- Navigation. We hide it in small screens. -->
+            <nav className="mdl-navigation">
+              <a className="task-text-primary task-header-button">Test</a>
+              <a className="task-text-primary task-header-button">To Do List</a>
+              <a className="task-text-primary task-header-button">Completed Tasks</a>
+            </nav>
+          </div>
+        </header>
+        <div className="mdl-layout__drawer" style="background: #FFD034;">
+          <span className="mdl-layout-title"><img src="../assets/maxthunder-logo.png" height="48" width="48"/></span>
+          <nav className="mdl-navigation">
+            <a className="task-text-primary task-header-button">To Do List</a>
+            <a className="task-text-primary task-header-button">Completed Tasks</a>
+          </nav>
+        </div>
+        <main className="mdl-layout__content">
+          <div className="page-content" style="min-height: 80%">
+            {/*<router-outlet></router-outlet>*/}
+          </div>
+          <footer className="mdl-mini-footer">
+            <div className="mdl-mini-footer__left-section">
+              <div className="mdl-logo"><img src="../assets/maxthunder-logo.png" height="48" width="48"/></div>
+              <ul className="mdl-mini-footer__link-list">
+                <li><a style="color: white">To Do List</a></li>
+                <li><a style="color: white">Completed Tasks</a></li>
+                <li><a href="https://github.com/maxthunder" style="color: white">Developer's GitHub Page</a></li>
+              </ul>
+            </div>
+          </footer>
+        </main>
+      </div>
   );
 }
 
